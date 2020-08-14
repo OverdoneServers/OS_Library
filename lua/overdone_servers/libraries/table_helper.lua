858e8b9ce5520aae097a1066b64e18e7e949759d
RyanTheTechMan <ryan@rivergroup.com>
2020-08-13 20:01:05 -0400

    Added TableHelper:InsertValue(value, tbl, ...)

--- lua/overdone_servers/libraries/table_helper.lua
+++ lua/overdone_servers/libraries/table_helper.lua
@@ -1,8 +1,8 @@
 OverdoneServers.TableHelper = {}
 
-function OverdoneServers.TableHelper:GetValue(table, ...) //Allows you to get a value wihin a table within a table (etc) while checking if its nil each time
+function OverdoneServers.TableHelper:GetValue(tbl, ...) //Allows you to get a value wihin a table within a table (etc) while checking if its nil each time
     local args = {...}
-    local tablePos = table
+    local tablePos = tbl
 
     for i=1, #args do
         if tablePos[args[i]] == nil then return nil end
@@ -12,9 +12,9 @@ function OverdoneServers.TableHelper:GetValue(table, ...) //Allows you to get a 
     return tablePos
 end
 
-function OverdoneServers.TableHelper:SetValue(value, table, ...) //Allows you to set a value at a given positon in a table and will create the keys that are required
+function OverdoneServers.TableHelper:SetValue(value, tbl, ...) //Allows you to set a value at a given positon in a table and will create the keys that are required
     local args = {...}
-    local tablePos = table
+    local tablePos = tbl
     
     for i=1, #args do
         if tablePos[args[i]] == nil then tablePos[args[i]] = {} end
@@ -22,6 +22,20 @@ function OverdoneServers.TableHelper:SetValue(value, table, ...) //Allows you to
             tablePos = tablePos[args[i]]
         else
             tablePos[args[i]] = value
+        end
+    end
+end
+
+function OverdoneServers.TableHelper:InsertValue(value, tbl, ...) //Allows you to insert a value at a given positon in a table and will create the keys that are required
+    local args = {...}
+    local tablePos = tbl
+    
+    for i=1, #args do
+        if tablePos[args[i]] == nil then tablePos[args[i]] = {} end
+        if #args > i then
+            tablePos = tablePos[args[i]]
+        else
+            return table.insert(tablePos[args[i]], value)
         end
     end
 end
