5b9908d4febae687ceee9afcc6b37c599f29b26e
RyanTheTechMan <ryan@rivergroup.com>
2023-05-26 17:35:05 -0400

    Second attempt of prioritized hooks. Still needs rewrite

--- lua/overdone_servers/libraries/prioritized_hooks.lua
+++ lua/overdone_servers/libraries/prioritized_hooks.lua
@@ -4,43 +4,30 @@ local HookPS = OverdoneServers.PrioritizedHooks
 local hookPrefix = OverdoneServers.LibraryModuleName .. ":PrioritizedHooks:"
 local maxPriority = 100000 -- This is the maximum priority that can be set. This is used to clamp the priority value.
 
-function HookPS:Initialize(eventName)
+function HookPS:Initialize(eventName, returns)
     self.Hooks = self.Hooks or {}
-    self.Hooks[eventName] = self.Hooks[eventName] or {Ordered = {}, Layered = {}}
+    self.Hooks[eventName] = self.Hooks[eventName] or {Ordered = {}, Returns = returns or {}}
 
     hook.Add(eventName, hookPrefix .. eventName, function(...)
-        local a,b = self:Call(eventName, ...)
-        print("Got val: " .. tostring(a) .. " and ")
-        PrintTable(b)
-        return a
+        return self:Call(eventName, ...)
     end)
 end
 
-function HookPS:Add(eventName, name, func, priority, offset)
+function HookPS:Add(eventName, name, func, priority) -- Add a bool to the end for isMergeable which will choose whether or not to merge the return values at the end.
     self:Initialize(eventName)
-    
-    priority = priority or 0
-    combineData = combineData or false -- Whether or not to combine the data from the layered hooks. If false, the data from the layered hooks will be discarded. (Can be table)
-    chainData = chainData or false -- Whether or not to chain the data from the previous hook. If false, the data from the previous hook will be discarded. (Can be table)
-    
-    priority = math.Clamp(priority, -maxPriority, maxPriority)
-    self.Hooks[eventName].Ordered[name] = {func = func, priority = priority, offset = offset or false}
+
+    priority = math.Clamp(priority or 0, -maxPriority, maxPriority)
+
+    self.Hooks[eventName].Ordered[name] = {func = func, priority = priority}
 
     self:SortHooks(eventName)
-end
-
-function HookPS:AddLayered(eventName, name, func)
-    self:Initialize(eventName)
-    
-    self.Hooks[eventName].Layered[name] = {func = func}
 end
 
 function HookPS:Remove(eventName, name)
     if self.Hooks and self.Hooks[eventName] then
         self.Hooks[eventName].Ordered[name] = nil
-        self.Hooks[eventName].Layered[name] = nil
 
-        if table.IsEmpty(self.Hooks[eventName].Ordered) and table.IsEmpty(self.Hooks[eventName].Layered) then
+        if table.IsEmpty(self.Hooks[eventName].Ordered) then
             hook.Remove(eventName, hookPrefix .. eventName)
             self.Hooks[eventName] = nil
         end
@@ -66,42 +53,47 @@ end
 
 function HookPS:Call(eventName, ...)
     if self.Hooks and self.Hooks[eventName] then
-        local baseArgs = {...}
-        local offsetArgs = {...}
+        local originalArgs = {...}
+        local previousReturn = {}
 
-        for _, name in ipairs(self.Hooks[eventName].OrderedSortedNames) do
-            local hook = self.Hooks[eventName].Ordered[name]
-            
-            if hook.offset then
-                local offset = {hook.func(unpack(baseArgs))}
-                for i=1, #baseArgs do
-                    if hook.offset == true or hook.offset[i] then
-                        if offset[i] == nil then
-                            -- Ignore this index if the offset is nil
-                        elseif isnumber(offsetArgs[i]) and isnumber(offset[i]) then
-                            offsetArgs[i] = offsetArgs[i] + offset[i]
-                        elseif isvector(offsetArgs[i]) and isvector(offset[i]) then
-                            offsetArgs[i] = offsetArgs[i] + offset[i]
-                        elseif isangle(offsetArgs[i]) and isangle(offset[i]) then
-                            offsetArgs[i].p = offsetArgs[i].p + offset[i].p
-                            offsetArgs[i].y = offsetArgs[i].y + offset[i].y
-                            offsetArgs[i].r = offsetArgs[i].r + offset[i].r
-                        else
-                            print("Error, tried to offset by type: " .. type(offsetArgs[i]) .. " and " .. type(offset[i]))
-                        end
-                    end
-                end
+        -- Initialize the previousReturn with default values
+        for name, value in pairs(self.Hooks[eventName].Returns) do
+            if isvector(value) then
+                previousReturn[name] = Vector(value.x, value.y, value.z)
+            elseif isangle(value) then
+                previousReturn[name] = Angle(value.pitch, value.yaw, value.roll)
+            elseif isnumber(value) then
+                previousReturn[name] = value
             else
-                baseArgs = {hook.func(unpack(offsetArgs))}
-                offsetArgs = table.Copy(baseArgs)
+                previousReturn[name] = nil
             end
         end
 
-        local layeredResults = {}
-        for name, hook in pairs(self.Hooks[eventName].Layered) do
-            table.insert(layeredResults, {hook.func(unpack(baseArgs))})
+        -- Create a sorted list of hook names based on priority.
+        local sortedNames = self.Hooks[eventName].OrderedSortedNames
+
+        for _, name in ipairs(sortedNames) do
+            local hook = self.Hooks[eventName].Ordered[name]
+
+            -- Call the function with the original arguments and the previous return value as separate parameters.
+            local newReturn = hook.func(originalArgs, previousReturn)
+
+            -- Update the previousReturn with the newReturn values, keeping the defaults for nil values
+            for key, value in pairs(newReturn) do
+                if value ~= nil then
+                    if isvector(previousReturn[key]) and isvector(value) then
+                        previousReturn[key] = previousReturn[key] + value
+                    elseif isangle(previousReturn[key]) and isangle(value) then
+                        previousReturn[key] = previousReturn[key] + value
+                    elseif isnumber(previousReturn[key]) and isnumber(value) then
+                        previousReturn[key] = previousReturn[key] + value
+                    else
+                        previousReturn[key] = value
+                    end
+                end
+            end
         end
 
-        return unpack(baseArgs), layeredResults
+        return previousReturn
     end
 end
